432964de3b67b43ef56e508cab1bb636283c397c
ottaviocoelho <ottaviocoelho@hotmail.com>
2020-10-02 19:59:28 -0300

    spectacle

--- CODIGO-FONTE/client/features/screens/buscar_screen/BuscarScreen.js
+++ CODIGO-FONTE/client/features/screens/buscar_screen/BuscarScreen.js
@@ -1,14 +1,43 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, {useState} from "react";
+import { View, Text, ScrollView, TextInput, StyleSheet } from "react-native";
 import { connect } from "react-redux";
 import PrimaryButton from "../../components/buttons/Button";
+import mock from './SpectaclesMock'
+import Spectacle from '../../components/Spectacle'
 
 const BuscarScreen = () => {
+
+  const [query, setQuery] = useState('')
+  // const [spectacles, setSpectacles] = useState([])
+  const [spectacles, setSpectacles] = useState(mock)
+
   return (
-    <View>
-      <Text>Em fase de desenvolvimento.</Text>
+    <View style={styles.container}>
+      <Text>Busque espetáculos.</Text>
+      <TextInput style={styles.input  } onChangeText={text => setQuery(text)}></TextInput>
+      <ScrollView>
+        {
+          spectacles.map(spectacle => <Spectacle spectacle={spectacle} />)
+        }
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24
+  },
+  input: {
+    height: 30,
+    backgroundColor: "white",
+    marginBottom: 10,
+    marginTop: 10,
+    borderRadius: 5,
+    borderLeftWidth: 2,
+    borderLeftColor: "blue",
+    paddingLeft: 5
+  }
+})
 
 export default BuscarScreen;
---
+++ CODIGO-FONTE/client/features/screens/buscar_screen/SpectaclesMock.js
@@ -0,0 +1,8 @@
+export default [{
+  title: 'Teste 1',
+  description: 'lorema sapioduh asiuh enua sdpaihj eap asuiodh eiu ads jashdkaj ',
+  price: 'R$10,00',
+  group: 'SADdane',
+  date: '23/12/1231',
+  hour: '23:00'
+}]
