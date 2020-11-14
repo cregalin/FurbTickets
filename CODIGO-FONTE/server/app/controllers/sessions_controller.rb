4180fdffb946808ef26a545887a6a83db6252841
Carol Regalin <carol_regalin@hotmail.com>
2020-11-14 13:14:20 -0300

    Ajustando formatação

--- CODIGO-FONTE/server/app/controllers/sessions_controller.rb
+++ CODIGO-FONTE/server/app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
 	def chairs
-		@chairs = Chair.select('chairs.status, chairs.id, chairs.reference, tickets.id ticket_id')
+		@chairs = Chair
+			.select('chairs.status, chairs.id, chairs.reference, tickets.id ticket_id')
 			.joins('LEFT JOIN sessions ON sessions.room_id = chairs.room_id ',
 				'LEFT JOIN tickets ON tickets.session_id = sessions.id AND chairs.id = tickets.chair_id')
 			.where(sessions: { id: params[:id] })
