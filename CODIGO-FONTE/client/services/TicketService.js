5962ee4803a406be34bebb63253e829a0026c4d4
ottaviocoelho <ottaviocoelho@hotmail.com>
2020-11-26 21:41:50 -0300

    save tickets

---
+++ CODIGO-FONTE/client/services/TicketService.js
@@ -0,0 +1,17 @@
+import {paramsToQuery} from 'utils/ShowListParamsUtils'
+
+const axios = require('axios');
+
+const fitubServer = axios.create({
+  baseURL: 'https://d7cf6136af83.ngrok.io',
+  timeout: 1000,
+  headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
+});
+
+export const confirmTickets = (payload) => {
+  return fitubServer.post('/tickets/confirm', payload)
+}
+
+export const saveTickets = (payload) => {
+  return fitubServer.post('/tickets', payload)
+}
